4aa8d6ab5330e9c9cd580f7f00096f63b7906133
Savary <timeo.savary@ecole.ensicaen.fr>
2025-06-19 14:29:01 +0100

    starting the rework of the project, wait option done, new root logic

--- src/outline_convert/renderer_text.py
+++ src/outline_convert/renderer_text.py
@@ -5,19 +5,39 @@ import xml.etree.ElementTree as ET
 from .utils import indent
 
 
-# -- RENDER TEXT ------------------------------------------------------------
-def render_text(node: Node, level: int = 0, indent_size: int = 2, strip_tags: bool = False) -> List[str]:
+def render_text(node: Node, indent_char: str, indent_size: int, level: int = 0, strip_tags: bool = False) -> List[str]:
     lines: List[str] = []
+    if level == 0:
+        lines.append(node.title)
     for child in node.children:
         title = child.title
         if strip_tags:
             title = ' '.join(part for part in title.split() if not part.startswith('#'))
-        prefix = ' ' * (level * indent_size) + '- ' + title
+        prefix = indent_char * (level * indent_size) + '- ' + title
 
         lines.append(prefix)
         if child.note:
             lines.append(' ' * (level * indent_size) + f'"{child.note}"')
-        lines.extend(render_text(child, level+1, indent_size, strip_tags=strip_tags))
+        lines.extend(render_text(child, indent_char=indent_char, indent_size=indent_size, level=level + 1,
+                                 strip_tags=strip_tags))
+    return lines
+
+
+
+# -- RENDER TEXT ------------------------------------------------------------
+def render_text_bis(node: Node, indent_char: str, indent_size: int, level: int = 0, strip_tags: bool = False) -> List[str]:
+    lines: List[str] = []
+
+    for child in node.children:
+        title = child.title
+        if strip_tags:
+            title = ' '.join(part for part in title.split() if not part.startswith('#'))
+        prefix = indent_char * (level * indent_size) + '- ' + title
+
+        lines.append(prefix)
+        if child.note:
+            lines.append(' ' * (level * indent_size) + f'"{child.note}"')
+        lines.extend(render_text(child, indent_char=indent_char,  indent_size=indent_size, level = level+1, strip_tags=strip_tags))
     return lines
 
 # -- RENDER OPML ------------------------------------------------------------
