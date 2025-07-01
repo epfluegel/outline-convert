7470dfa63c929cfac4ca000032c9455de494eacf
Savary <timeo.savary@ecole.ensicaen.fr>
2025-07-01 11:34:17 +0100

    Files removed from gitignore

--- src/outline_convert/renderer_ppt.py
+++
@@ -1,5 +0,0 @@
-from .models import Node
-import argparse
-
-def render_ppt(node: Node, args: argparse.Namespace):
-    return None
--- src/outline_convert/renderer_rtf.py
+++
@@ -1,5 +0,0 @@
-from .models import Node
-import argparse
-
-def render_rtf(node: Node, args: argparse.Namespace):
-    return None
